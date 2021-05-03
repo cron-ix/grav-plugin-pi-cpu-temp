39aaeab0c10b1f117c3236b0a84b7a7be1b30b3d
cron-ix <74101164+cron-ix@users.noreply.github.com>
2021-05-03 17:27:32 +0200

    Add files via upload

---
+++ twig/PiCpuTempTwigExtension.php
@@ -0,0 +1,21 @@
+<?php
+namespace Grav\Plugin;
+class PiCpuTempTwigExtension extends \Twig_Extension
+{
+	public function getName()
+	{
+		return 'PiCpuTempTwigExtension';
+	}
+	public function getFunctions()
+	{
+		return [
+			new \Twig_SimpleFunction('piCpuTemp', [$this, 'piCpuTempFunction'])
+		];
+	}
+	public function piCpuTempFunction()
+	{
+		$cpu = shell_exec("cat /sys/class/thermal/thermal_zone0/temp");
+		return number_format($cpu/1000, 1, ',', '.') . " °C";
+	}
+}
+

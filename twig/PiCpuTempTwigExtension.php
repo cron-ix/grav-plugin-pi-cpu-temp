cc6fbcab6a9490214811fe03f2d519e5c5b2e9d7
Thomas Schroeder <Thomas.Schroeder@vrax.de>
2021-05-06 14:23:38 +0200

    using file_get_contents instead of cat

--- twig/PiCpuTempTwigExtension.php
+++ twig/PiCpuTempTwigExtension.php
@@ -26,16 +26,18 @@ class PiCpuTempTwigExtension extends \Twig_Extension
 	 */
 	public function piCpuTempFunction()
 	{
-		try {
-			// try to read the cpu temp from the RasPi sepcific path
-			$cpu = shell_exec("cat /sys/class/thermal/thermal_zone0/temp");
-			// return the formatted temperature in Degree Celsius
-			return number_format($cpu/1000, 1, ',', '.') . " °C";
-		} catch (Exception $ex) {
-			// return error message
-			// return $ex->getMessage();
-			return "An error occured while reading from `/sys/class/thermal/thermal_zone0/temp`, see [README.md](https://github.com/cron-ix/grav-plugin-pi-cpu-temp#readme) for further information.";
+		$sensor_path = '/sys/class/thermal/thermal_zone0/temp';
+
+		// read content
+		$sensor_value = @file_get_contents($sensor_path);
+		// do the error handling
+		if ($sensor_value === FALSE) {
+			$return_value = "An error occured while reading from `/sys/class/thermal/thermal_zone0/temp`, see [README.md](https://github.com/cron-ix/grav-plugin-pi-cpu-temp#readme) for further information.";
 		}
+		elseif (is_numeric($sensor_value)) {
+			$return_value = number_format($cpu/1000, 1, ',', '.') . " °C";
+		}
+		return $return_value;
 	}
 }
 
